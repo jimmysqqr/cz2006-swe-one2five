6bed7a3e685452b402b871abf6f50b0245fcd0c5
hoanghd2000 <hoanghd2000@gmail.com>
2022-03-24 10:21:39 +0800

    Finished searchingRentedFlats, draft for managing savedFlats

---
+++ controllers/savedFlats.js
@@ -0,0 +1,116 @@
+const SavedFlat = require('../models/SavedFlat');
+const isHDB = require('../models/HDB');
+
+// Get all saved flats
+const getAllSavedFlats = (req, res) => {
+    // res.send('Get all saved flats');
+    SavedFlat.getAllSavedFlats((err, data) => {
+        if (err) {
+            res.status(500).json({
+                message: "Some error occurred while fetching saved flats"
+            })
+        }
+        else res.status(200).json(data);
+    })
+}
+
+// Get saved flat by id
+const getSavedFlat = (req, res) => {
+    // res.json({ id:req.params.id });
+    SavedFlat.getById(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).json({
+                    message: `No saved flat with if ${req.params.id}`
+                });
+            }
+            else {
+                res.status(500).json({
+                    message: `Error occurred while fetching saved flat id ${req.params.id}`
+                });
+            }
+        }
+        else res.status(200).json(data);
+    })
+}
+
+// Create a new saved flat
+const createSavedFlat = (req, res) => {
+    // es.json(req.body);
+    if (!req.body) {
+        res.status(400).json({
+            message: "Input cannot be empty!"
+        });
+        return;
+    }
+
+    // Check if the inputted HDB address is valid
+    isHDB(req.body.block, req.body.street_name, (err, result) => {
+        if (err) {
+            res.status(500).json({
+                message: `Error occurred while checking for valid HDB address`
+            });
+            return;
+        }
+        if (!result) {
+            res.status(400).json({
+                message: "Invalid HDB address!"
+            });
+            return;
+        }
+    });
+    console.log("dumb")
+
+    // Create the new saved flat object
+    const newSavedFlat = new SavedFlat({
+        id: null,
+        town: null,
+        block: req.body.block,
+        street_name: req.body.street_name,
+        flat_type: req.body.flat_type,
+        monthly_rent: null
+    });
+
+    // Save the new saved flat in the database
+    SavedFlat.create(newSavedFlat, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                message: "Some error occurred while creating a new saved flat"
+            });
+        }
+        else res.status(200).json({
+            message: "Save flat!",
+            data: data
+        });
+    });
+}
+
+// Delete a saved flat
+const deleteSavedFlat = (req, res) => {
+    // res.send('delete saved flat of id x');
+    SavedFlat.remove(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).json({
+                    message: `No saved flat with if ${req.params.id}`
+                });
+            }
+            else {
+                res.status(500).json({
+                    message: `Error occurred while deleting saved flat id ${req.params.id}`
+                });
+            }
+        }
+        else res.status(200).json({
+            message: `Saved Flat id ${req.params.id} was deleted successfully`
+        });
+    });
+}
+
+// Export controllers
+module.exports = {
+    getAllSavedFlats,
+    getSavedFlat,
+    createSavedFlat,
+    deleteSavedFlat
+};
---
+++ controllers/searchRentedFlats.js
@@ -0,0 +1,104 @@
+const RentedOutFlat = require('../models/RentedOutFlat');
+
+// Search for rented-out flats based on town, flat_type, price, and nearby amenity
+const searchRentedFlats = (req, res) => {
+    const { town, flatType, numericFilters, amenityType, amenityDist } = req.query;
+    // console.log(town, flatType, numericFilters, amenityType, amenityDist);
+
+    // // If no search filter is inputted, return the whole list of rented-out flats
+    // if (!(town || flatType || numericFilters || amenityType || amenityDist)) {
+    //     RentedOutFlat.getAll((err, data) => {
+    //         if (err)
+    //             res.status(500).json(
+    //                 {
+    //                     message: err.message || "Some errors occured while fetching rent-out flats"
+    //                 }
+    //             );
+    //         else res.status(200).json(data);
+    //     })
+    //     return;
+    // }
+
+    let loPrBound = null;
+    let hiPrBound = null;
+    
+    // Process numericFilters
+    if (numericFilters) {
+        let numericSeparateFilters = numericFilters.split(',');
+        for (let i = 0; i < numericSeparateFilters.length; i++) {
+            let filter = numericSeparateFilters[i];
+
+            // Get higher price bound
+            if (filter.search(/^price<=/g) != -1){
+                hiPrBound = Number(filter.replace(/^price<=/g, ''));
+
+                if(isNaN(hiPrBound)) {
+                    console.log("Error: Please enter a numeric higher price bound");
+                    return;
+                }
+            }
+
+            // Get lower price bound
+            else if (filter.search(/^price>=/g) != -1){
+                loPrBound = Number(filter.replace(/^price>=/g, ''));
+
+                if(isNaN(loPrBound)) {
+                    console.log("Error: Please enter a numeric lower price bound");
+                    return;
+                }
+            }
+        }
+    }
+
+    // Perform the search
+    RentedOutFlat.search(town, flatType, loPrBound, hiPrBound, amenityType, amenityDist, (err,data) => {
+        if (err)
+            res.status(500).json(
+                {
+                    message: err.message || "Some errors occured while searching for rent-out flats according to user's filters"
+                }
+            );
+        else res.status(200).json(data);
+    })
+}
+
+
+// Get all rented-out flats
+const getAllRentedFlats = (req, res) => {
+    RentedOutFlat.getAll((err, data) => {
+        if (err)
+            res.status(500).json(
+                {
+                    message: err.message || "Some errors occured while fetching rent-out flats"
+                }
+            );
+        else res.status(200).json(data);
+    })
+}
+
+// Get a rented-out flat by id
+const getRentedFlat = (req, res) => {
+    // res.send('rented-out flat of id x');
+    RentedOutFlat.getById(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).json({
+                    message: `No rented-out flat with id ${req.params.id}`
+                });
+            }
+            else {
+                res.status(500).json({
+                    message: `Error occurred while fetching rented-out flat with id ${req.params.id}`
+                });
+            }
+        }
+        else res.status(200).json(data);
+    });
+}
+
+// Export controllers
+module.exports = {
+    getAllRentedFlats,
+    getRentedFlat,
+    searchRentedFlats
+};
