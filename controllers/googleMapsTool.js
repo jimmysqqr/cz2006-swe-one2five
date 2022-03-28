e5da67d3730a00b42979aac560f94e9c0693ef33
jimmysqqr <77312754+jimmysqqr@users.noreply.github.com>
2022-03-29 02:17:17 +0800

    GMap Amenity & Price functions

--- controllers/googleMapsTool.js
+++ controllers/googleMapsTool.js
@@ -1,13 +1,57 @@
+const { response } = require('express');
+
 // Method to search for rented-out flats based on a nearby amenity
 const searchByAmenity = (rentedOutFlatList, amenityType, amenityDist) => {
     /*Function that takes in a list of rented-out flats (json) and return the rented-out flats (json) in that list that has a 
     nearby amenity of amenityType (e.g. hsopital, school, etc.) within amenityDist m/km (whichever suitable)
     */
+    
+    // only return AFTER finishing for loop & receiving results from findAllNearbyAmenities()
+    async function filterAsync(){
+        let filteredList = []; 
+
+        for (let flat of rentedOutFlatList){
+            let response = await findAllNearbyAmenities(flat, amenityType, amenityDist);
+            if (response.status === 'OK'){
+                filteredList.push(flat);
+            } 
+        };
+ 
+        return filteredList;
+    }
+
+    // only return result AFTER filterList() is done
+    return filterAsync().then(data => { return data; })
+
 }
 
 // Method to find all nearby amenities (based on the defined list of amenities) of a particular flat
-const findAllNearbyAmenities = (flatCoords) => {
-    // flatCoords: longtitude and latitude
+const findAllNearbyAmenities = (flatCoords, amenityType = null, amenityDist = 1000) => { 
+    // flatCoords: longtitude and latitude // assume nearby is 1km
+        /* im assuming it's 
+        flatCoords = {
+            lat:123,
+            lng:456
+        }
+        */
+
+    // actual url to call 
+    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?' +
+            'location=' + flatCoords.lat + '%2C' + flatCoords.lng +
+            '&radius=' + amenityDist +
+            (amenityType ? '&type=' + amenityType : '') +
+            '&key=AIzaSyB4C3UfSaq-9qQXITAIHjCFCUqBWP2nUzM';
+
+    // use axios to talk to G map api, only return AFTER receiving response
+    var axios = require('axios');   
+    async function callApiAsync() {
+        const response = await axios.get(url)
+        return response.data
+    }
+
+    // only return result AFTER callAPI() is done
+    return callApiAsync().then(data => { return data; });
+
 }
 
 // Method to find the coordinates of a particular address
@@ -19,3 +63,54 @@ const findCoords = (address) => {
 const calcDistance = (coords0, coords1) => {
 
 }
+
+
+
+
+
+
+
+
+// DEBUG //
+
+/* 
+// test data
+rentedOutFlatList = [
+    { // NS
+        lat:1.3471940782229364,
+        lng:103.68077682078855
+    },
+    { // Pioneer mall
+        lat:1.3418851208330858, 
+        lng:103.69738694571976
+    }
+];
+
+amenityType = 'gym';
+amenityDist = '200' // meters
+*/
+
+/* 
+// (1) findAllNearbyAmenities
+
+async function test(){
+    const a = await findAllNearbyAmenities(rentedOutFlatList[0]);
+    console.log("the function returns:");
+    console.log(a);
+    return a;
+}
+
+test()
+*/
+
+/* 
+// (2) searchByAmenity
+
+async function test(){
+    const b = await searchByAmenity(rentedOutFlatList, amenityType, amenityDist);
+     console.log("the function returns:");
+     console.log(b);
+}
+
+test();
+*/
--- controllers/priceCalculator.js
+++ controllers/priceCalculator.js
@@ -1,14 +1,49 @@
 // Method to calculate the average price of a list of flats
 const avgCalc = (flatList) => {
-
-}
+	let sum = 0;
+	for (let flat of flatList) {
+		sum += flat.monthly_rent;
+	}
+	return sum / flatList.length;
+};
 
 // Method to calculate the 10th and 90th percentile price of a list of flats
 const percentileCalc = (flatList) => {
-    
-}
+	const asc = (arr) => arr.sort((a, b) => a - b); // sort array ascending
+	const priceList = asc(flatList.map((flat) => flat.monthly_rent));
+
+	const pctl = (sorted, p) => {
+		const pos = (sorted.length - 1) * p;
+		const base = Math.floor(pos);
+		const rest = pos - base;
+		if (sorted[base + 1] !== undefined) {
+			return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
+		} else {
+			return sorted[base];
+		}
+	};
+
+	return [pctl(priceList, 0.1), pctl(priceList, 0.9)];
+};
 
 // Method to predict the future price of a specific flat based on rented-out flats that are similar to the Target Flat
-const predictPrice = (flatList) => {
+const predictPrice = (flatList) => {};
 
-}
+/*
+rentedOutFlatList = [
+    { // NS
+        monthly_rent:3000,
+        lat:1.3471940782229364,
+        lng:103.68077682078855
+    },
+    { // Pioneer mall
+        monthly_rent:5000,
+        lat:1.3418851208330858, 
+        lng:103.69738694571976
+    }
+];
+
+console.log(avgCalc(rentedOutFlatList));
+console.log(percentileCalc(rentedOutFlatList));
+
+*/
