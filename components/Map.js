45eae24bcf127f291b3050a954724a5a1b9aea89
siti-nur-ummaira <84258243+siti-nur-ummaira@users.noreply.github.com>
2022-03-30 15:41:13 +0800

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,13 @@
 import styles from "./flatInfo.module.scss";
 
+// blue marker for amenities
+const image = {
+  url: "https://www.clipartmax.com/png/full/95-954602_google-maps-marker-blue.png",
+  scaledSize: new google.maps.Size(20,32),
+  //origin: new google.maps.Point(0,0),
+  //anchor: new google.maps.Point(0,32),
+};
+
 export function Map() {
   const middleSG = { lat: 1.34791, lng: 103.83288 };
 
@@ -9,5 +17,24 @@ export function Map() {
     fullScreenControl: true, // allow user to enlarge map
     gestureHandling: "cooperative", // only zoom in when ctrl button pressed (if not, greedy)
     //restriction: { north: northLat, south: southLat, west: -180, east: 180 }; // to restrict how much user zooms out
+  });
+
+}
+
+// Method to add markers to locations
+addMarker = (icon, mapName, latLng) => { // icon is boolean, mapName is string, latLng in curly braces
+  if (icon) {
+    new google.maps.Marker ({
+      map: mapName,
+      position: latLng,
+      icon: image,
+      //title? when mouse hovers over will appear
     });
+  } else {
+    new google.maps.Marker ({
+      map: mapName,
+      position: latLng,
+      //title
+    });
+  }
 }
