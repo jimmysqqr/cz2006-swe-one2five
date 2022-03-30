4faf8244f24790bde0c38438286aa9fcfad2ac82
siti-nur-ummaira <84258243+siti-nur-ummaira@users.noreply.github.com>
2022-03-30 12:12:00 +0800

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,13 @@
 import styles from "./flatInfo.module.scss";
 
 export function Map() {
-  return "Hi Im map";
+  const middleSG = { lat: 1.34791, lng: 103.83288 };
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: middleSG,
+    fullScreenControl: true, // allow user to enlarge map
+    gestureHandling: "cooperative", // only zoom in when ctrl button pressed (if not, greedy)
+    //restriction: { north: northLat, south: southLat, west: -180, east: 180 }; // to restrict how much user zooms out
+    });
 }
