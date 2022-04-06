c2f2a73e2991e084a6a4d943bc4d9006b8703d2f
lihaan <45350323+lihaan@users.noreply.github.com>
2022-04-06 08:14:01 +0800

    try out next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    AMENITIES_GGMapsAPIKey: "AIzaSyB4C3UfSaq-9qQXITAIHjCFCUqBWP2nUzM",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
